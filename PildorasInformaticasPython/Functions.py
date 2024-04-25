e2663368fbf72d3f19892b2d213cbf00de41c7ea
walter ROA SERRANO <wroaserrano@gmail.com>
2024-04-25 21:43:11 +0200

    functions with & without parameters

---
+++ PildorasInformaticasPython/Functions.py
@@ -0,0 +1,25 @@
+#function begin
+def function():
+    print("here you can write something")#it is necessary to indent 
+#you need to always call the function
+function()
+function()
+function()     
+
+
+def Suma():
+    num1=5
+    num2=7
+    print(num1+num2)
+
+Suma()
+
+#with parameters
+
+def SumaWithParameters(num1, num2):
+    print(num1+num2)
+
+SumaWithParameters(5,8)
+SumaWithParameters(2,3)
+SumaWithParameters(35,358)
+

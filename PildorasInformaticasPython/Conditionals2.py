3d4181ca83551431755d6ab395e7df972c0b1575
walter ROA SERRANO <wroaserrano@gmail.com>
2024-05-30 22:39:18 +0200

    conditionals part 2

---
+++ PildorasInformaticasPython/Conditionals2.py
@@ -0,0 +1,32 @@
+#Conditionals
+
+'''
+Do not forget to Encode the file to UTF-8
+
+Instructions :
+
+IF 
+
+ELSE
+
+ELIF -> o evaluate many conditions
+
+
+you cannot have one else without if
+'''
+
+print('access verification')
+
+user_age=int(input('enter your age : '))
+
+if user_age < 18 :
+    print('warning ! you can go')
+
+elif user_age > 100 :
+    print('you are not human')
+
+else :
+    print('you can go')
+
+print('the program has end')
+    

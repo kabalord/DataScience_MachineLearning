c917a213b8777105aa0d34ad7dab9ad7458b191b
walter ROA SERRANO <wroaserrano@gmail.com>
2024-06-18 22:00:15 +0200

    loops part 1

---
+++ PildorasInformaticasPython/Loops1.py
@@ -0,0 +1,37 @@
+# Loops
+
+'''
+Loops
+
+the usefulness is to repeat one or more lines of code several times.
+
+Types of Loops :
+
+Determined:
+    - They are executed a certain number of times.
+    - It is known a priori how many times the code inside the loop is going to be executed.
+
+
+Indeterminate:
+    - They are executed an indeterminate number of times.
+    - It is not known a priori how many times the code inside the loop will be executed.
+    - The number of times it will be executed will depend on the circumstances during the execution of the program.
+
+'''
+#Do not forget to Encode the file to UTF-8
+
+'''
+Loop for (Determined) syntax :
+
+for variable in element to traverse 
+    loop body
+'''
+
+for i in [1,2,3]: #statement of the loop
+    print('hola') #body of the loop
+    
+for i in ['spring', 'summer', 'autumn', 'winter',]: 
+    print(i) 
+
+
+    
